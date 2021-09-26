8fab587ed8933a7c9fd9ba0b2c89f8df0c202f4a
Vinayak <vinayakchaurasia99@gmail.com>
2021-09-26 22:41:47 +0530

    Added notification event supplier

--- src/main/java/com/vinayak/microservice/notificationmicroservice/NotificationMicroserviceApplication.java
+++ src/main/java/com/vinayak/microservice/notificationmicroservice/NotificationMicroserviceApplication.java
@@ -2,12 +2,21 @@ package com.vinayak.microservice.notificationmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class NotificationMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationMicroserviceApplication.class, args);
+	}
+
+	@Bean
+	public Consumer<Message<String>> notificationEventSupplier() {
+		return message -> new EmailSender().sendEmail(message.getPayload());
 	}
 
 }
